b09653b65603ed7bc46b5fdd9dd4a14d792f60aa
QalbiAlmustikaM <qalbimustika25@gmail.com>
2023-10-11 23:00:57 +0800

    Day3

---
+++ Day3/gvd.java
@@ -0,0 +1,16 @@
+package Day3;
+
+
+public class gvd {
+     public static void main(String[] args) {
+        int angkaPertama = 5;
+        int angkaKedua = 3;
+
+        // Melakukan penjumlahan
+        int hasil = angkaPertama + angkaKedua;
+
+        // Menampilkan hasil
+        System.out.println("Hasil penjumlahan: " + hasil);
+    }
+    
+}
