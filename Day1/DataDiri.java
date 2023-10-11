8af912e0f084df107edee806d85cb05e6b19a06d
QalbiAlmustikaM <qalbimustika25@gmail.com>
2023-10-11 22:31:42 +0800

    100Days

---
+++ Day1/DataDiri.java
@@ -0,0 +1,29 @@
+package Day1;
+
+public class DataDiri {
+    
+     public static void main(String[] args) {
+        String nama = "Qalbi Almustika M";
+        int umur = 20;
+        double tinggi = 145.0;
+        float berat = 43.5f;
+        char jenisKelamin = 'P';
+        boolean menikah = false;
+        
+
+        //Menampilkan data diri
+        System.out.println("Data Diri:");
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur: " + umur + " tahun");
+        System.out.println("Tinggi: " + tinggi + " cm");
+        System.out.println("Berat: " + berat + " kg");
+        System.out.println("Jenis Kelamin: " + jenisKelamin);
+        System.out.println("Menikah: " + menikah);
+        
+    }
+}
+    
+
+
+    
+
